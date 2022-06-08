5a9e832307a227ae45583c9ee8490941a69277c4
Bulzaj <ernestsado94@gmail.com>
2022-06-08 15:06:50 +0200

    feat - add typography in hero section

--- src/components/hero-section/HeroSection.js
+++ src/components/hero-section/HeroSection.js
@@ -1,8 +1,42 @@
-import { Box } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import classes from "./heroSection.module.css";
 
 const HeroSection = function () {
-  return <Box className={classes.wrapper}></Box>;
+  const buttonClickHandler = function () {
+    console.log("clicked");
+  };
+
+  return (
+    <Box
+      className={classes.wrapper}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Box display="flex" flexDirection="column" justifyContent="center">
+        <Typography
+          variant="h1"
+          gutterBottom
+          sx={{
+            fontWeight: "600",
+            background: "-webkit-linear-gradient(bottom,  #ccc, #222)",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+          }}
+        >
+          Halo Companion App
+        </Typography>
+        <Button
+          variant="contained"
+          size="large"
+          sx={{ margin: "auto" }}
+          onClick={buttonClickHandler}
+        >
+          Get started
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default HeroSection;
