45e3d8265157783ef38f3fc6f47635a804688e90
bulzaj <ernestsado94@gmail.com>
2022-06-09 21:08:50 +0200

    feat - color navbar when scroll down

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,16 @@
-import { AppBar, Toolbar } from "@mui/material";
+import { AppBar, Toolbar, useScrollTrigger } from "@mui/material";
 import Logotype from "../logotype/Logotype";
+import { blueGrey } from "@mui/material/colors";
 
 const Navbar = function () {
+  const trigger = useScrollTrigger();
+
   return (
-    <AppBar position="fixed" color="transparent" elevation={0}>
+    <AppBar
+      position="fixed"
+      color={trigger ? "primary" : "transparent"}
+      elevation={trigger ? 4 : 0}
+    >
       <Toolbar>
         <Logotype light />
       </Toolbar>
