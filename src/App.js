85ada869dede51bcd427e7f218d93d92ea777d53
bulzaj <ernestsado94@gmail.com>
2022-06-09 16:35:14 +0200

    feat - add auto font sizing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import { CssBaseline } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  ThemeProvider,
+  createTheme,
+  responsiveFontSizes,
+} from "@mui/material/styles";
 import Navbar from "./components/navbar/Navbar";
 import LandingPage from "./pages/landing-page/LandingPage";
 
+let theme = createTheme({});
+theme = responsiveFontSizes(theme);
+
 function App() {
   return (
-    <div>
-      <CssBaseline />
-      <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <CssBaseline />
+        <Navbar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   );
 }
 
