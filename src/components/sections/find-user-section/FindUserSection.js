2540aa7f305741fe755b5285dd03fcd67469e74c
bulzaj <ernestsado94@gmail.com>
2022-06-12 19:34:43 +0200

    feat - add input validation

--- src/components/sections/find-user-section/FindUserSection.js
+++ src/components/sections/find-user-section/FindUserSection.js
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import { Box, Container, Button, TextField, Typography } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
+const HELPER_TEXT = "Field could not be empty";
+
 const FindUserSection = function () {
+  const [inputValue, setInputValue] = useState("");
+  const [focusOut, setFocusOut] = useState(false);
+
   const handleSubmit = function (e) {
     e.preventDefault();
     console.log("submited");
   };
 
+  const handleInputChange = function (e) {
+    e.preventDefault();
+    setInputValue(e.target.value);
+  };
+
+  const handleBlur = function (e) {
+    e.preventDefault();
+    setFocusOut(true);
+  };
+
   return (
-    <Container component="section" maxWidth="lg">
+    <Container id="find-user-section" component="section" maxWidth="lg">
       <Box
         sx={{
           marginTop: 9,
@@ -23,18 +39,23 @@ const FindUserSection = function () {
         </Typography>
         <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
           <TextField
+            onBlur={handleBlur}
+            onChange={handleInputChange}
             id="playertag"
             margin="normal"
             fullWidth
             name="playerstag"
             type="text"
             label="Player's Gametag"
+            error={!inputValue && focusOut}
+            helperText={!inputValue && focusOut && HELPER_TEXT}
           />
           <Button
             variant="contained"
             fullWidth
             endIcon={<SearchIcon />}
             sx={{ mt: 3, mb: 2 }}
+            disabled={!inputValue}
           >
             Search
           </Button>
